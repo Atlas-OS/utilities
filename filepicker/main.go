f3f8031c39665ac3c47b24188e7ac2aca3bd3f9a
Zusier <zusier@pm.me>
2021-11-20 12:11:44 -0800

    Add ability to change file extension/filter

--- filepicker/main.go
+++ filepicker/main.go
@@ -2,19 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/harry1453/go-common-file-dialog/cfd"
 	"github.com/harry1453/go-common-file-dialog/cfdutil"
 )
 
 func main() {
+	// Check for extension arg
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: filepicker <ext>")
+		os.Exit(1)
+	}
+	var ext = os.Args[1]
 	result, err := cfdutil.ShowOpenFileDialog(cfd.DialogConfig{
-		Title:       "Open An Executable", // Title of the dialog window
-		Role:        "AtlasFilePick",      // used to differentiate between different file picker dialogs
-		FileFilters: []cfd.FileFilter{{DisplayName: "Executable Files (*.exe)", Pattern: "*.exe"}},
+		Title:       "Select a File", // Title of the dialog window
+		Role:        "AtlasFilePick", // used to differentiate between different file picker dialogs
+		FileFilters: []cfd.FileFilter{{DisplayName: ext + " Files", Pattern: "*." + ext}},
 	})
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
 	fmt.Println(result)
 }
